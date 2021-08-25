602d9686e60226be654ef324948fd2c5047f9c65
VV <85766798+Vedant-cll@users.noreply.github.com>
2021-08-25 18:29:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL('https://www.localsamosa.com/wp-content/uploads/2020/07/rakhi-1-1068x713.jpg',function(Img){
+block_image_object=Img;
+
+block_image_object.scaleToWidth(800);
+block_image_object.scaleToHeight(710);
+block_image_object.set({
+top:0,
+left:0
+});
+canvas.add(block_image_object);
+})	
+	}
+
+function playSound(){
+x.play();	
+}
